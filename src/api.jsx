d2fd1a0d12ea4c75f6e5d1946b01986089087688
Aathiskumar2004 <aathiskumaraathis@gmail.com>
2025-11-20 11:40:01 +0530

    Edited

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://express-jwt-vl8z.onrender.com",
+  baseURL: "https://express-jwt-4h94.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
